9c5af03fee44311dc187a7ee566972115b343d2c
Rémi Ratjczak <remi.ratajczak@gmail.com>
2018-04-20 10:49:26 +0200

    Fixed optional python tsv file writter

--- createTSVFromFolder.py
+++ createTSVFromFolder.py
@@ -7,16 +7,19 @@ images = []
 extension = '.jpg'
 for root, dirs, files in os.walk(path):
     for file in files:
-        if file.endswith(extension):
+        if file.endswith(extension): #only get files with required extension
             images.append(file)
-            print(file)
 
+# Open a tsv file according to the path name and write it as follow
+# [id] [image]
+#  1   name1.jpg
+#  2   name2.jpg
 with open(path+'.tsv', 'w') as tsvfile:
-    writer = csv.writer(tsvfile, delimiter='\t')
     id=0
-    for record in files:
+    writer = csv.writer(tsvfile, delimiter='\t') #writer
+    for image in images:
         if id == 0:
             writer.writerow(['id', 'image'])
         else:
-            writer.writerow([id, file])
+            writer.writerow([id, image])
         id = id+1
