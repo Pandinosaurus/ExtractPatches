118b049e8b1f433ca75ab7dfd2c2d9c9e5d26383
Rémi Ratjczak <remi.ratajczak@gmail.com>
2018-04-20 10:44:42 +0200

    Python optional tsv file writter

--- createTSVFromFolder.py
+++ createTSVFromFolder.py
@@ -1,8 +1,22 @@
 import os
+import csv
 
+# List images with ".jpg" extension in given path
+path = './data/outputColorGray_bis'
 images = []
-for root, dirs, files in os.walk(r'data/outputColorGray_bis/'):
+extension = '.jpg'
+for root, dirs, files in os.walk(path):
     for file in files:
-        if file.endswith('.png'):
+        if file.endswith(extension):
             images.append(file)
-			print(file)
+            print(file)
+
+with open(path+'.tsv', 'w') as tsvfile:
+    writer = csv.writer(tsvfile, delimiter='\t')
+    id=0
+    for record in files:
+        if id == 0:
+            writer.writerow(['id', 'image'])
+        else:
+            writer.writerow([id, file])
+        id = id+1
