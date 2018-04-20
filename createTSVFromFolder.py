811e9e2a8a04e2b622da2bf26e09181a5f0e81e8
Rémi Ratjczak <remi.ratajczak@gmail.com>
2018-04-20 10:58:22 +0200

    Added parser to optional Python tsv script

--- createTSVFromFolder.py
+++ createTSVFromFolder.py
@@ -1,20 +1,36 @@
 import os
 import csv
+import argparse
+
+# Add parsing arguments
+parser = argparse.ArgumentParser(description='Write a tsv file for each file in the given path this/is/a/path')
+parser.add_argument('-path',
+                    '--path',
+                    required=False,
+                    default='./data/outputColorGray_bis',
+                    help='the path to the folder that contains the files to record (e.g. ./data/images)')
+parser.add_argument('-extension',
+                    '--extension',
+                    default='.jpg',
+                    required=False,
+                    help='the extension of the files to record (e.g. .jpg)')
+
+# Parse
+args = parser.parse_args()
+
 
 # List images with ".jpg" extension in given path
-path = './data/outputColorGray_bis'
 images = []
-extension = '.jpg'
-for root, dirs, files in os.walk(path):
+for root, dirs, files in os.walk(args.path):
     for file in files:
-        if file.endswith(extension): #only get files with required extension
+        if file.endswith(args.extension): #only get files with required extension
             images.append(file)
 
 # Open a tsv file according to the path name and write it as follow
 # [id] [image]
-#  1   name1.jpg
-#  2   name2.jpg
-with open(path+'.tsv', 'w') as tsvfile:
+#   1  name1.jpg
+#   2  name2.jpg
+with open(args.path+'.tsv', 'w') as tsvfile:
     id=0
     writer = csv.writer(tsvfile, delimiter='\t') #writer
     for image in images:
